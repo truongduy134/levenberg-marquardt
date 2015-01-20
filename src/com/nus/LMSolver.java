dd6bc3cfecf92d744fad0299991413ce0854567d
Nguyen Truong Duy <truongduy134@yahoo.com>
2015-01-20 23:54:28 +0800

    Skeleton for LMSolver class

---
+++ src/com/nus/LMSolver.java
@@ -0,0 +1,61 @@
+package com.nus;
+
+import Jama.Matrix;
+
+/**
+ * Created by duy on 20/1/15.
+ */
+public class LMSolver {
+  // Configuration parameters for Levenberg-Marquadt algorithm
+  private double lambda;
+  private int maxNumIter;
+  private double termEpsilon;
+
+  private IErrorFunc errorFunc;
+
+  public LMSolver(IErrorFunc inErrorFunc) {
+    this.lambda = 10;
+    this.maxNumIter = 10;
+    this.termEpsilon = 0.00001;
+    this.errorFunc = inErrorFunc;
+  }
+
+  public LMSolver(
+      double lambda,
+      int maxNumIter,
+      double termEpsilon,
+      IErrorFunc errorFunc) {
+    this.lambda = lambda;
+    this.maxNumIter = maxNumIter;
+    this.termEpsilon = termEpsilon;
+    this.errorFunc = errorFunc;
+  }
+
+  public double getLambda() {
+    return lambda;
+  }
+
+  public void setLambda(double lambda) {
+    this.lambda = lambda;
+  }
+
+  public int getMaxNumIter() {
+    return maxNumIter;
+  }
+
+  public IErrorFunc getErrorFunc() {
+    return errorFunc;
+  }
+
+  public void setMaxNumIter(int maxNumIter) {
+    this.maxNumIter = maxNumIter;
+  }
+
+  public double getTermEpsilon() {
+    return termEpsilon;
+  }
+
+  public void setTermEpsilon(double termEpsilon) {
+    this.termEpsilon = termEpsilon;
+  }
+}
