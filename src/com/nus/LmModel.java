79c85cc52cd84bb1a4a92761364ec50fd9a63690
Nguyen Truong Duy <truongduy134@yahoo.com>
2015-01-30 01:27:12 +0800

    Simplify the way user provides jacobian and hessian

---
+++ src/com/nus/LmModel.java
@@ -0,0 +1,40 @@
+package com.nus;
+
+/**
+ * Created by duy on 27/1/15.
+ */
+public interface LmModel {
+  /**
+   * Gets the vector of real numbers containing measured output data,
+   */
+  public double[] getMeasuredData();
+
+  /**
+   * Evaluates the model's estimated output for the k-th input data that
+   * corresponds to the parameter vector
+   * @param dataIdx The index of the input data
+   * @param optParams A vector of real values of parameters in the model
+   * @return Estimated output value produced by the model
+   */
+  public double eval(int dataIdx, double[] optParams);
+
+  /**
+   * Computes the model's Jacobian vector for the k-th input data that
+   * corresponds to the parameter vector
+   *
+   * @param dataIdx The index of the input data
+   * @param optParams A vector of real values of parameters in the model
+   * @return Jacobian vector of the model for the specified input data
+   */
+  public double[] jacobian(int dataIdx, double[] optParams);
+
+  /**
+   * Computes the model's Hessian matrix for the k-th input data that
+   * corresponds to the parameter vector
+   *
+   * @param dataIdx The index of the input data
+   * @param optParams A vector of real values of parameters in the model
+   * @return Hessian matrix of the model for the specified input data
+   */
+  public double[][] hessian(int dataIdx, double[] optParams);
+}
---
+++ src/com/nus/LmSumSquaresError.java
@@ -0,0 +1,103 @@
+package com.nus;
+
+import Jama.Matrix;
+
+/**
+ * Created by duy on 27/1/15.
+ */
+public class LmSumSquaresError implements LmModelError {
+  private LmModel model;
+
+  public LmSumSquaresError(LmModel model) {
+    this.model = model;
+  }
+
+  public LmModel getModel() {
+    return model;
+  }
+
+  public void setModel(LmModel model) {
+    this.model = model;
+  }
+
+  /**
+   * Evaluates the error function with input optimization parameter values
+   *
+   * @param optParams A vector of real values of parameters used in optimizing
+   *                  the error function
+   * @return Double value of the error function
+   */
+  public double eval(double[] optParams) {
+    double[] measuredOutputs = model.getMeasuredData();
+    int numData = measuredOutputs.length;
+
+    double errorSum = 0.0;
+    for (int i = 0; i < numData; ++i) {
+      double error = measuredOutputs[i] - model.eval(i, optParams);
+      errorSum += error * error;
+    }
+
+    return 0.5 * errorSum;
+  }
+
+  /**
+   * Computes the Jacobian vector of the error function with input
+   * optimization parameter values
+   *
+   * @param optParams A vector of real values of parameters used in optimizing
+   *                  the error function
+   * @return Jacobian vector of the error function
+   */
+  public double[] jacobian(double[] optParams) {
+    double[] measuredOutputs = model.getMeasuredData();
+    int numData = measuredOutputs.length;
+    int numParams = optParams.length;
+
+    double[] jVector = new double[numParams];
+    for (int i = 0; i < numParams; ++i) {
+      jVector[i] = 0.0;
+    }
+
+    for (int i = 0; i < numData; ++i) {
+      double[] modelJacobian = model.jacobian(i, optParams);
+      double error = measuredOutputs[i] - model.eval(i, optParams);
+
+      for (int k = 0; k < numParams; k++) {
+        jVector[k] -= modelJacobian[k] * error;
+      }
+    }
+
+    return jVector;
+  }
+
+  /**
+   * Computes the Hessian matrix of the error function with input
+   * optimization parameter values
+   *
+   * @param optParams A vector of real values of parameters used in optimizing
+   *                  the error function
+   * @return Hessian matrix of the error function
+   */
+  public double[][] hessian(double[] optParams) {
+    double[] measuredOutputs = model.getMeasuredData();
+    int numData = measuredOutputs.length;
+    int numParams = optParams.length;
+
+    double[][] jVectors = new double[numData][numParams];
+    for (int i = 0; i < numData; ++i) {
+      jVectors[i] = model.jacobian(i, optParams);
+    }
+    Matrix jacobianMat = new Matrix(jVectors);
+    Matrix hessianMat = jacobianMat.transpose().times(jacobianMat);
+    for (int i = 0; i < numData; ++i) {
+      double error = measuredOutputs[i] - model.eval(i, optParams);
+      Matrix modelHessian = new Matrix(model.hessian(i, optParams));
+      hessianMat.minusEquals(modelHessian.times(error));
+      /*for (int k = 0; k < 2; k++)
+        for (int j = 0; j < 2; j++)
+          System.out.println(hessianMat.getArray()[k][j]);*/
+    }
+
+    return hessianMat.getArrayCopy();
+  }
+}
